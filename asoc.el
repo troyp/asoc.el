785881063b6b13104a099a517cddfb9ac3f94975
Troy Pracy <troyp7@gmail.com>
2017-09-15 08:16:41 +1000

    Add commentary section (from README)

--- asoc.el
+++ asoc.el
@@ -23,6 +23,14 @@
 
 ;;; Commentary:
 
+;; Emacs Lisp provides only minimal facilities for dealing with alists, and
+;; processing them using standard list functions is awkward and tedious. `asoc`
+;; provides a complete API for handling association lists. In addition to basic
+;; functions for creating, accessing and modifying alists, it provides mapping,
+;; filtering and folding facilities in both regular and anaphoric variants, a
+;; looping construct analogous to `dolist`, and a special variable for
+;; configuring the equality predicate used by `asoc` operations.
+
 ;;
 
 ;;; Code:
